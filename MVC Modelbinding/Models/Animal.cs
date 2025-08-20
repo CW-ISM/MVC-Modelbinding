cabb8b74661b3d79ef660096fb84a72a4c1bb79b
CW-ISM <cwismgitism@gmail.com>
2025-08-20 04:36:09 -0700

    Update

--- MVC Modelbinding/Models/Animal.cs
+++ MVC Modelbinding/Models/Animal.cs
@@ -17,7 +17,7 @@ public class Animal
     /// </summary>
     [Required(ErrorMessage = "You must provide the animal's common name")]
     [StringLength(128)]
-    [RegularExpression("[A-Za-z ]", ErrorMessage = "Common name can only contain letters and spaceses (A-Z, a-z, ")]
+    [RegularExpression("^[A-Za-z\\s]+$", ErrorMessage = "Common name can only contain letters and spaceses (A-Z, a-z)")]
     public required string CommonName { get; set; } = string.Empty;
 
     /// <summary>
@@ -25,7 +25,7 @@ public class Animal
     /// </summary>
     [Required(ErrorMessage = "You must provide a class the animal belongs to")]
     [StringLength(128)]
-    [RegularExpression("[A-Za-z ]", ErrorMessage = "Class can only contain letters and spaceses (A-Z, a-z, ")]
+    [RegularExpression("^[A-Za-z\\s]+$", ErrorMessage = "Class can only contain letters and spaceses (A-Z, a-z)")]
     public required string Class { get; set; } = string.Empty;
 
     /// <summary>
@@ -33,7 +33,7 @@ public class Animal
     /// </summary>
     [Required(ErrorMessage = "You must provide a family the animal belongs to")]
     [StringLength(128)]
-    [RegularExpression("[A-Za-z ]", ErrorMessage = "Family can only contain letters and spaceses (A-Z, a-z, ")]
+    [RegularExpression("^[A-Za-z\\s]+$", ErrorMessage = "Family can only contain letters and spaces (A-Z, a-z)")]
     public required string Family { get; set; } = string.Empty;
 
     /// <summary>
@@ -41,15 +41,14 @@ public class Animal
     /// </summary>
     [Required(ErrorMessage = "You must provide a genus the animal belongs to")]
     [StringLength(128)]
-    [RegularExpression("[A-Za-z ]", ErrorMessage = "Genus can only contain letters and spaceses (A-Z, a-z, ")]
+    [RegularExpression("^[A-Za-z\\s]+$", ErrorMessage = "Genus can only contain letters and spaces (A-Z, a-z)")]
     public required string Genus { get; set; } = string.Empty;
 
     /// <summary>
     /// Gets or sets the lifespan range of the animal in years.
     /// </summary>
     [Required(ErrorMessage = "You must provide the average lifespan of the animal")]
-    [Range(0, int.MaxValue)]
-    public required Range LifeSpan { get; set; }
+    public required LifeSpan LifeSpan { get; set; }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Animal"/> class.
@@ -60,7 +59,7 @@ public class Animal
     /// <param name="family">The family of the animal.</param>
     /// <param name="genus">The genus of the animal.</param>
     /// <param name="lifeSpan">The lifespan range of the animal.</param>
-    public Animal(int id, string commonName, string @class, string family, string genus, Range lifeSpan)
+    public Animal(int id, string commonName, string @class, string family, string genus, LifeSpan lifeSpan)
     {
         Id = id;
         CommonName = commonName;
@@ -69,4 +68,15 @@ public class Animal
         Genus = genus;
         LifeSpan = lifeSpan;
     }
+}
+
+public class LifeSpan
+{
+    [Range(0, 1000)]
+    public UInt16 Min { get; set; }
+
+    [Range(0, 1000)]
+    public UInt16 Max { get; set; }
+
+    public override string ToString() => $"{Min} - {Max} years";
 }
