88911746f9fc12535f8d37b1652e1430a22cbb55
CW-ISM <cwismgitism@gmail.com>
2025-08-20 11:55:03 -0700

    Created an AnimalController

---
+++ MVC Modelbinding/Controllers/AnimalController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Modelbinding.Models;
+
+namespace MVC_Modelbinding.Controllers;
+
+public class AnimalController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Animal animal)
+    {
+        if (ModelState.IsValid)
+        {
+            TempData["Message"] = "Animal was added successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View(animal);
+    }
+}
